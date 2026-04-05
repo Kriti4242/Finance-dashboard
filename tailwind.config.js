a9d89a87cda12cca99c80b2950517cef971a1493
KRITI <kritiverma2424@gmail.com>
2026-04-05 15:58:24 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,11 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  darkMode: 'class',
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      boxShadow: {
-        soft: '0 10px 30px -16px rgba(15, 23, 42, 0.25)',
-      },
-    },
+    extend: {},
   },
   plugins: [],
-};
+}
