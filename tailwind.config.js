181528acab03d3646405e1259fb05397c38ea393
KRITI <kritiverma2424@gmail.com>
2026-04-05 16:00:28 +0530

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+       boxShadow: {
+        soft: '0 10px 30px -16px rgba(15, 23, 42, 0.25)',
+      },
+    },
   },
   plugins: [],
 }
